2ddd5847de68fa388cfaa3ad3e7354a425b40157
autumn <doingnone@gmail.com>
2019-04-26 21:44:54 +0900

    change scripts

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tar --exclude='node_modules' -cvf dummybear-api.tar ./
+tar --exclude='node_modules' --exclude='build' -cvf dummybear-api.tar ./
 scp -i /Users/autumn/.ssh/aws_ec2_t2micro.pem dummybear-api.tar ec2-user@15.164.45.155:~
 rm dummybear-api.tar
 
